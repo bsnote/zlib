51770325a2e86d810180431d44394e9b13d2f255
Andrei Sedoi <bsnote@gmail.com>
2013-03-06 19:55:46 +0200

    create mips.sh

---
+++ mips.sh
@@ -0,0 +1,7 @@
+if [[ "$PATH" != *"/usr/local/mips-2012.09/bin"* ]]; then
+	export PATH="/usr/local/mips-2012.09/bin:$PATH"
+fi
+
+CHOST=mips-linux-gnu CFLAGS="-mips32r2 -EL -mabi=32" ./configure
+make
+
