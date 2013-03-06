08dda4925f8694ad397bdb06d496654c9a2432cf
Andrei Sedoi <bsnote@gmail.com>
2013-03-06 21:08:22 +0200

    rename: out -> sysroot

--- mips.sh
+++ mips.sh
@@ -2,7 +2,7 @@ if [[ "$PATH" != *"/usr/local/mips-2012.09/bin"* ]]; then
 	export PATH="/usr/local/mips-2012.09/bin:$PATH"
 fi
 
-CHOST=mips-linux-gnu CFLAGS="-mips32r2 -EL -mabi=32" prefix=./out ./configure
+CHOST=mips-linux-gnu CFLAGS="-mips32r2 -EL -mabi=32" prefix=./sysroot ./configure
 make
 make install
 
