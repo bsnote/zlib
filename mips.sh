f0215cc81f32b1381a982acd63f467c5e5ebf02a
Andrei Sedoi <bsnote@gmail.com>
2013-03-07 23:28:42 +0200

    rename sysroot to zlib-1.2.7-1.mipsel

--- mips.sh
+++ mips.sh
@@ -5,7 +5,7 @@ if [[ "$PATH" != *"/usr/local/mips-2012.09/bin"* ]]; then
 	export PATH="/usr/local/mips-2012.09/bin:$PATH"
 fi
 
-CHOST=mips-linux-gnu CFLAGS="-mips32r2 -EL -mabi=32" prefix=./sysroot ./configure
+CHOST=mips-linux-gnu CFLAGS="-mips32r2 -EL -mabi=32" prefix=./zlib-1.2.7-1.mipsel ./configure
 make
 make install
 
